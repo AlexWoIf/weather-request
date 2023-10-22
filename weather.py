2cbb2c465389960563849e4ff849a13edd3d047a
AlexWoIf <alexey.barannikov@gmail.com>
2023-10-22 14:12:08 +0300

    Update weather.py

--- weather.py
+++ weather.py
@@ -1,10 +1,15 @@
 # -*- coding: utf-8 -*-
 import requests
 
+
+def request_weather(place):
+    url = f'https://wttr.in/~{place}?nTqMm&lang=ru'
+    response = requests.get(url)
+    response.raise_for_status()
+    return response.text
+
+
 if __name__ == '__main__':
     places = ['Лондон', 'Шереметьево', 'Череповец']
     for place in places:
-        url = f'https://wttr.in/~{place}?nTqMm&lang=ru'
-        response = requests.get(url)
-        response.raise_for_status()
-        print(response.text)
+        print(request_weather(place))
