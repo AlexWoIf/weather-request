55baf160fba5f516562044d3892d0cfea4b05b58
AlexWoIf <alexey.barannikov@gmail.com>
2023-10-22 12:03:01 +0300

    Task finished

---
+++ weather.py
@@ -0,0 +1,9 @@
+import requests
+
+if __name__ == '__main__':
+    places = ['Лондон', 'Шереметьево', 'Череповец']
+    for place in places:
+        url = f'https://wttr.in/~{place}?nTqMm&lang=ru'
+        response = requests.get(url)
+        response.raise_for_status()
+        print(response.text)
