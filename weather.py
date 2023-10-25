099d39de32850fc35f74e2904236c137670fc30c
AlexWoIf <alexey.barannikov@gmail.com>
2023-10-25 11:51:16 +0300

    Update weather.py

--- weather.py
+++ weather.py
@@ -3,7 +3,7 @@ import requests
 
 
 def request_weather(place):
-    url = f'https://wttr.in/~{place}?nTqMm&lang=ru'
+    url = f'https://wttr.in/{place}?nTqMm&lang=ru'
     response = requests.get(url)
     response.raise_for_status()
     return response.text
